72f532e731b3bb47188ddef68df99149aaddf300
britaszy <britaszy@gmail.com>
2020-06-13 19:58:47 -0600

    Brita's changes

--- Project3/index.js
+++ Project3/index.js
@@ -1,28 +1,25 @@
-const port = 3001;
-const express = require("express");
-const app = express();
-const path = require("path");
-const bodyParser = require("body-parser");
-const knex = require("knex") ({
+let express = require("express");
+let app = express();
+let path = require("path");
+let port = 3000;
+let bodyParser = require("body-parser");
+let knex = require("knex")({
     client: "sqlite3",
     connection: {
-        filename: "MusicLibrary.db" 
+        filename: "MusicLibrary.db"
     },
-    useNullAsDefault: true  
+    useNullAsDefault: true
 });
-
-
-app.listen(port, () => {
-console.log("Started server at port " + port);
-});
-
+app.listen(port, function(){
+    console.log("I'm listening")
+})
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    knex.select("*").from("Songs").orderBy("SongName").then(song =>{ 
+    knex.select("SongID","SongName","ArtistID","YearReleased").from("Songs").orderBy("SongID").then(song =>{ 
         res.render("index", {song: song} );
     }).catch(err => {
         console.log(err);
@@ -36,16 +33,15 @@ app.post('/DeleteSong/:id', (req, res) => {
     })
 });
 
-app.get('/AddSong', (req,res) => {
-    res.render('addsong');
-});
+app.get("/addSong", (req,  res) => {
+    res.render("addSong");
+})
 
-app.post('/AddSong', (req, res) => { 
-    console.log(req.body);
-    knex('Songs').insert(req.body).then(song => {
-        res.redirect('/');
-    });
-});
+app.post("/addSong", (req, res) =>{
+    knex("Songs").insert(req.body).then(song => {
+        res.redirect("/");
+    })
+})
 
 app.get('/EditSong/:SongID', (req, res) => {
     res.render('editsong', {id: req.params.SongID});
@@ -55,21 +51,6 @@ app.post('/EditSong/:SongID', (req, res) => {
     knex('Songs').where({ SongID: req.params.SongID }).update({
         SongName: req.body.SongName, ArtistID: req.body.ArtistID, 
         YearReleased: req.body.YearReleased }).then(song => {
-            res.redirect('/');
-        });  
-});
-
-app.post('/StartOver', (req, res) => {
-        knex('Songs').where(true).del().then(
-            res.redirect('SetUp'));
-});
-
-app.get('/SetUp', (req, res) => {
-    knex('Songs').insert([
-        {SongName: "Bohemian Rhapsody", ArtistID: "QUEEN", YearReleased: "1975"},
-        {SongName: "Don't Stop Believing", ArtistID: "JOURNEY", YearReleased: "1981"},
-        {SongName: "Hey Jude", ArtistID: "BEATLES", YearReleased: "1968"}
-    ]).then(song => {
             res.redirect('/');
         });  
 });
